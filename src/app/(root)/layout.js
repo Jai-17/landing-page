d06be385c864f48343a53ab819909199f766829b
Jai-17 <madhukarjai9@gmail.com>
2025-06-29 02:28:25 +0530

    init

---
+++ src/app/(root)/layout.js
@@ -0,0 +1,16 @@
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const RootLayout = ({children}) => {
+  return (
+    <div className='bg-[#FCFBFA] pattern min-h-screen'>
+      <nav>
+        <Navbar />
+      </nav>
+      {children}
+    </div>
+  )
+}
+
+export default RootLayout
